3924254ce334c19d1d9e1b074dac612f58cac3bb
Yeiber Cano <yeibercano@gmail.com>
2016-07-14 22:45:55 -0700

    connect mongodb to mongoose

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const app = express();
 const router = require('./router');
+const mongoose = require('mongoose');
+
+//DB setup
+mongoose.connect('mongodb://localhost:auth/auth');
 
 //App setup
 app.use(morgan('combined'));
