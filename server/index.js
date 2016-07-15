5d4989761d70ca567f23b6d6048ad170f0a9fc09
Yeiber Cano <yeibercano@gmail.com>
2016-07-14 21:59:47 -0700

    add middleware morgan and bosy-parser

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+
+//App setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+
+//Server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('server listening on:', port);
