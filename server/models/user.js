ea962c178d0cd1f30c049afdc8cd24dd66d5d093
Yeiber Cano <yeibercano@gmail.com>
2016-07-15 00:02:32 -0700

    fix typo in password and add hook pre saving user

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 //Define model
 const userSchema = new Schema({
 	email: { type: String, unique: true, lowercase: true },
-	passsword: String
+	password: String
+});
+
+// ecrypt password
+userSchema.pre('save', function(next) {
+	const user = this;
+	
+	bcrypt.genSalt(10, function(err, salt){
+		if (err) { return next(err) };
+
+		bcrypt.hash(user.password, salt, null, function(err, hash) {
+		  if (err) { return next(err) };
+		  
+		  user.password = hash;
+		  next();	
+		});
+	});
 });
 
 //Create the model class
