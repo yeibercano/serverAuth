c9a982d1eb9b9e769f93a28a0bab6e62dffe5238
Yeiber Cano <yeibercano@gmail.com>
2016-07-15 14:40:31 -0700

    add logic to check user pass

--- server/services/passport.js
+++ server/services/passport.js
@@ -3,6 +3,19 @@ const User = require('../models/user');
 const config = require('../../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+
+const localOptions = { usernameField: 'email'};
+const localLogin = new LocalStrategy(localOptions, function (email, password, done) {
+	User.findOne({ email: email}, function(err, user) {
+		if (err) { return done(err) };
+		if (!user) { return done(null, dalse) };
+
+		// compare pass
+		
+	})
+});
 
 //setup options
 const jwtOptions = {
