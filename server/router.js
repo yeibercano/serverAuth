0ecf00c50df8a9ac573948fdc2f304de9339d0ec
Yeiber Cano <yeibercano@gmail.com>
2016-07-17 22:29:09 -0700

    change message featured

--- server/router.js
+++ server/router.js
@@ -7,7 +7,7 @@ const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
 	app.get('/', requireAuth, function(req, res) {
-		res.send({ authorized: 'you made it' });
+		res.send({ message: 'access featured route' });
 	});
 	app.post('/signin', requireSignin, Authentication.signin);
 	app.post('/signup', Authentication.signup);
