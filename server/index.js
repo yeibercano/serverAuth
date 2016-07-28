8a99120673af113bc4a2dc1efc85e28a238ffaca
Yeiber Cano <yeibercano@gmail.com>
2016-07-27 23:52:42 -0700

    use middleware cors and set online database

--- server/index.js
+++ server/index.js
@@ -8,11 +8,18 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 //DB setup
-mongoose.connect('mongodb://localhost:auth/auth');
+// mongoose.connect('mongodb://localhost:auth/auth');
+//mongoose connection
+mongoose.connect('mongodb://protected:123@ds031835.mlab.com:31835/protected'); 
 
+app.use(cors());
 //App setup
 app.use(morgan('combined'));
-app.use(cors());
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
